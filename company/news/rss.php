52b4057fe77db8a845a7d2656606b4ef3ae4d202
Helga13 <oliatrubina@yandex.ru>
2016-05-18 17:45:33 +0300

    general settings

---
+++ company/news/rss.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?><rss version="2.0">
+<channel>
+<title>Новости</title>
+<link>http://provance.flex.by</link>
+<description></description>
+<lastBuildDate>Wed, 18 May 2016 15:20:19 +0300</lastBuildDate>
+<ttl>60</ttl>
+</channel>
+</rss>
