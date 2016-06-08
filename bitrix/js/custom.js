981da6dd9ee60db61054acd33c8f8fe7f11ef715
Helga13 <oliatrubina@yandex.ru>
2016-06-08 18:16:35 +0300

    adaptive

--- bitrix/js/custom.js
+++ bitrix/js/custom.js
@@ -84,6 +84,23 @@ $('.faq.list .item .q').hover(function(){
 	
 	
 	
+	$('#tabSet').scrollTabs({
+      scroll_distance: 350,
+      scroll_duration: 350,
+      left_arrow_size: 20,
+      right_arrow_size: 20,
+      click_callback: function(e){
+        var val = $(this).attr('rel');
+        if(val){
+          window.location.href = val;
+        }
+      }
+    });
+	
+
+	
+	
+	
 	
 	
 	
