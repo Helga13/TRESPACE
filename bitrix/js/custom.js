d1bbf72535cfb2c3c09807f5b8f3ea6a08546f82
Helga13 <oliatrubina@yandex.ru>
2016-06-09 14:47:06 +0300

    cart480

--- bitrix/js/custom.js
+++ bitrix/js/custom.js
@@ -97,6 +97,22 @@ $('.faq.list .item .q').hover(function(){
       }
     });
 	
+	$('#tabSetCard').scrollTabs({
+				  scroll_distance: 350,
+				  scroll_duration: 350,
+				  left_arrow_size: 20,
+				  right_arrow_size: 20,
+				  click_callback: function(e){
+					var val = $(this).attr('rel');
+					if(val){
+					  window.location.href = val;
+					}
+				  }
+				});
+	
+	
+	
+	
 
 	
 	
