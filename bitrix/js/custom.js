1960c5d73104d240112d4fa62a91a24179d02ebb
Helga13 <oliatrubina@yandex.ru>
2016-06-14 09:06:27 +0300

    header. ie

--- bitrix/js/custom.js
+++ bitrix/js/custom.js
@@ -4,31 +4,14 @@ $(document).ready(function(){
 	
 	$('.child.submenu').siblings('a').addClass('after');
 	
-	//main slider
 	
-//	$('.item.fancy').on('click', function() {
-//		var attr_name = $(this).attr('rel');
-//		var attr_length = $(this).attr('rel', attr_name).parents('.slides.slides-fancy').find('li').length;
-//		$(".item.fancy").fancybox({
-//			maxWidth: '800',
-//			scrolling: 'no',
-//			index: 1,
-//			padding: 0,
-//			tpl: {
-//				wrap: '<div class="fancybox-wrap" tabIndex="1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"><div class="counter"><div class="count_elem"><span class="current-slide"></span><span class="all"></span></div></div></div></div></div></div>',
-//			},
-//			afterShow: function () {
-//				$('.all').text(attr_length);            
-//				$('.current-slide').text($.fancybox.current.index + 1);
-//
-//			},
-//			helpers: {
-//				overlay: {
-//					locked: false
-//				}
-//			}
-//		});
-//	});
+	//slider count
+	
+//function showCountSlider{
+//	var totalSlides = $('.tab_slider_wrapp.s_WqTNqQpWko .tabs_content > li.cur > .flex-viewport > .tabs_slider > li').length;
+//								
+//};
+
 	
 	
 	
